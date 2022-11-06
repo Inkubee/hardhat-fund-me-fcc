932b2dec33f6438f3c1540b028762471bfeccfd6
Inkubee <inkubee18@gmail.com>
2022-11-06 10:02:24 +0100

    initial commit

---
+++ contracts/FundMe.sol
@@ -0,0 +1,118 @@
+// 1. License
+// SPDX-License-Identifier: MIT
+
+// 2. Version of solidity
+pragma solidity ^0.8.8;
+
+// 3. Import
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "./PriceConverter.sol";
+import "hardhat/console.sol";
+
+// 4. Error Codes
+error FundMe__NotOwner();
+
+// 5. Interfaces, Libraries, Contracts
+
+/**
+ * @title
+ * @author
+ * @notice
+ * @dev
+ * etc....
+ */
+// 6. Contract
+contract FundMe {
+    // 1. Type of declerations
+    using PriceConverter for uint256;
+
+    // 2. State variable : arrays, mapping,  etc...
+    // To store a var we put s_ for storage before the var and i_ for immtable
+    mapping(address => uint256) public s_addressToAmountFunded;
+    address[] public s_funders;
+
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address private immutable i_owner;
+    uint256 public constant MINIMUM_USD = 50 * 10**18;
+    // AggregatorV3Interface private s_priceFeed;
+
+    AggregatorV3Interface public s_priceFeed;
+
+    constructor(address priceFeedAddress) {
+        i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeedAddress);
+    }
+
+    /**
+     * @notice This function funds this contract
+     * @dev This implements price feed as our library
+     */
+
+    function fund() public payable {
+        require(
+            msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD,
+            "You need to spend more ETH!"
+        );
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
+        s_addressToAmountFunded[msg.sender] += msg.value;
+        s_funders.push(msg.sender);
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
+    }
+
+    function withdraw() public payable onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < s_funders.length;
+            funderIndex++
+        ) {
+            address funder = s_funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        require(callSuccess, "Call failed");
+    }
+
+    function cheaperWithdraw() public payable onlyOwner {
+        address[] memory funders = s_funders;
+        // mapping can't be in memory, sorry!
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
+            address funder = funders[funderIndex];
+            s_addressToAmountFunded[funder] = 0;
+        }
+        s_funders = new address[](0);
+        (bool success, ) = i_owner.call{value: address(this).balance}("");
+        require(success);
+    }
+
+    function getOwner() public view returns (address) {
+        return i_owner;
+    }
+
+    function getFunder(uint256 index) public view returns (address) {
+        return s_funders[index];
+    }
+
+    function getAddressToAmoundFunded(address funder)
+        public
+        view
+        returns (uint256)
+    {
+        return s_addressToAmountFunded[funder];
+    }
+
+    function getPriceFeed() public view returns (AggregatorV3Interface) {
+        return s_priceFeed;
+    }
+}
---
+++ test/staging/Fund.me.staging.test.js
@@ -0,0 +1,31 @@
+const { utils } = require("ethers");
+const { getNamedAccounts, ethers, network } = require("hardhat");
+const { developmentChains } = require("../../helper-hardhat-config");
+const { assert } = require(chai);
+
+/*
+let variable = false
+let someVar = ? "yes" : "no"
+            ==
+if (variable) {someVar="yes"} else {someVar="no"}
+*/
+
+developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("FundMe", async function () {
+          let fundMe;
+          let deployer;
+          const sendValue = utils.parseEther("1");
+          beforeEach(async function () {
+              deployer = (await getNamedAccounts()).deployer;
+              fundMe = await ethers.getContract("FundMe", deployer);
+          });
+          it("allows people to fund and withdraw", async function () {
+              await fundMe.fundMe({ value: sendValue });
+              await fundMe.withdraw();
+              const endingBalance = await fundMe.provider.getBalance(
+                  fundMe.address
+              );
+              assert.equal(endingBalance.toString(), "0");
+          });
+      });
